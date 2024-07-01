7e255fa9c9c7a4ba22446233cce83bb94711c1c6
neeramrutia <neer.amrutia@gmail.com>
2024-07-02 01:06:38 +0530

    added ytdl-core

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,61 +1,77 @@
-import { Text, Container, Anchor, Button, ActionIcon, rem, TextInput, useMantineTheme } from "@mantine/core";
+import {
+  Text,
+  Container,
+  Anchor,
+  Button,
+  ActionIcon,
+  rem,
+  TextInput,
+  useMantineTheme,
+  AspectRatio,
+  Blockquote,
+} from "@mantine/core";
 import React, { useState } from "react";
 import { MdOutlineArrowDownward } from "react-icons/md";
 import { DiGithubBadge } from "react-icons/di";
 import { Link } from "react-scroll";
 import "../Styles/About.css";
-import { IconSearch, IconArrowRight } from "@tabler/icons-react";
-const isBrowser = typeof window !== "undefined"
-const donload = async(link:String) =>{
-  const res = await fetch(`http://localhost:8000/api/download?link=${link}`);
-  const data = await res.json();
-  console.log(data);
-}
+import { IconSearch, IconArrowRight, IconInfoCircle } from "@tabler/icons-react";
+import axios, { AxiosResponse } from "axios";
+const isBrowser = typeof window !== "undefined";
+
 const About = () => {
   const theme = useMantineTheme();
-  const [url , setUrl] = useState("");
-  
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState<AxiosResponse>();
+  const icon = <IconInfoCircle />;
+  const donload = async (link: String) => {
+    const data = await axios.get(
+      `http://localhost:8000/api/download?link=${link}`
+    );
+    setData(data);
+    setUrl("");
+  };
   return (
-    <section id="about">
-      <Container fluid mt={"xl"}>
-        <div className="about-content" style={{ backgroundColor: "#fff4d5" }}>
-          <div style={{ marginBottom: 15 }}>
-            <Text tt={"uppercase"} fw={500} c="yellow">
-              MADE WITH GATSBY AND MANTINE
-            </Text>
-          </div>
+    <>
+      <section id="about">
+        <Container fluid mt={"xl"}>
+          <div className="about-content" style={{ backgroundColor: "#fff4d5" }}>
+            <div style={{ marginBottom: 15 }}>
+              <Text tt={"uppercase"} fw={500} c="yellow">
+                MADE WITH GATSBY AND MANTINE
+              </Text>
+            </div>
 
-          <div style={{ marginBottom: 15 }}>
-            <Text>
-              {/* <MediaQuery query="(max-width: 768px)" styles={{ fontSize: '2.8rem !important' }}> */}
-              <h1 className="title">Youtube Video Downloader</h1>
-              {/* </MediaQuery> */}
-            </Text>
-          </div>
+            <div style={{ marginBottom: 15 }}>
+              <Text>
+                {/* <MediaQuery query="(max-width: 768px)" styles={{ fontSize: '2.8rem !important' }}> */}
+                <h1 className="title">Youtube Video Downloader</h1>
+                {/* </MediaQuery> */}
+              </Text>
+            </div>
 
-          <div style={{ marginBottom: 25 }}>
-            <Text size="xl" c="black">
-              Simple showcase of this powerful and well implemented library
-              called <Anchor href="https://mantine.dev/">Mantine</Anchor> with{" "}
-              <Anchor href="https://www.gatsbyjs.com/">Gatsby</Anchor>.
-            </Text>
-          </div>
+            <div style={{ marginBottom: 25 }}>
+              <Text size="xl" c="black">
+                Simple showcase of this powerful and well implemented library
+                called <Anchor href="https://mantine.dev/">Mantine</Anchor> with{" "}
+                <Anchor href="https://www.gatsbyjs.com/">Gatsby</Anchor>.
+              </Text>
+            </div>
 
-          <div className="buttons">
-            <Link to="section-five" smooth duration={500}>
-              <Button
-                color="yellow"
-                rightSection={<MdOutlineArrowDownward size={16} />}
-                radius="lg"
-                size="md"
-              >
-                Tell me more
-              </Button>
-            </Link>
-            <Link to="github.com/neeramrutia">
-             {
-                isBrowser && (
-                    <Button
+            <div className="buttons">
+              <Link to="section-five" smooth duration={500}>
+                <Button
+                  color="yellow"
+                  rightSection={<MdOutlineArrowDownward size={16} />}
+                  radius="lg"
+                  size="md"
+                >
+                  Tell me more
+                </Button>
+              </Link>
+              <Link to="github.com/neeramrutia">
+                {isBrowser && (
+                  <Button
                     onClick={() => {
                       window.location.href = "https://github.com/neeramrutia";
                     }}
@@ -66,47 +82,119 @@ const About = () => {
                   >
                     Github
                   </Button>
-                )
-             }
-            </Link>
-          </div>
-          <form onSubmit={async(e)=>{e.preventDefault();await donload(url)}}>
-          <div> 
-            <TextInput
-              pt={"lg"}
-              radius="md"
-              size="md"
-              placeholder="Enter a youtube URL"
-              rightSectionWidth={42}
-              leftSection={
-                <IconSearch
-                  style={{ width: rem(18), height: rem(18) }}
-                  stroke={1.5}
+                )}
+              </Link>
+            </div>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                await donload(url);
+              }}
+            >
+              <div>
+                <TextInput
+                  pt={"lg"}
+                  radius="md"
+                  size="md"
+                  placeholder="Enter a youtube URL"
+                  rightSectionWidth={42}
+                  value={url}
+                  leftSection={
+                    <IconSearch
+                      style={{ width: rem(18), height: rem(18) }}
+                      stroke={1.5}
+                    />
+                  }
+                  onSubmit={async () => {
+                    await donload(url);
+                  }}
+                  onChange={(e) => {
+                    setUrl(e.target.value);
+                  }}
+                  rightSection={
+                    <Link to="video-section" smooth duration={500}>
+                    <ActionIcon
+                      size={32}
+                      radius="xl"
+                      color={theme.colors.yellow[4]}
+                      variant="filled"
+                      onClick={async () => {
+                        await donload(url);
+                      }}
+                    >
+                      <IconArrowRight
+                        style={{ width: rem(18), height: rem(18) }}
+                        stroke={1.5}
+                      />
+                    </ActionIcon>
+                    </Link>
+                  }
                 />
-              }
-              onSubmit={async()=>{await donload(url)}}
-              onChange={(e)=>{setUrl(e.target.value)}}
-              rightSection={
-                <ActionIcon
-                  size={32}
-                  radius="xl"
-                  color={theme.colors.yellow[4]}
-                  variant="filled"
-                  onClick={async()=>{await donload(url)}}
-                >
-                  <IconArrowRight
-                    style={{ width: rem(18), height: rem(18) }}
-                    stroke={1.5}
-                  />
-                </ActionIcon>
-              }
-            />
+              </div>
+            </form>
           </div>
-          </form>
-        </div>
-      </Container>
-    </section>
+        </Container>
+      </section>
+      <section >
+        <Container id="video-section" size={"xs"}>
+        {
+        data ? <>
+          <AspectRatio>
+      <iframe
+        src={`${data?.data.url}`}
+        title="YouTube video player"
+        style={{ border: 0 }}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      />
+      {
+        data?.data.info.map((formatName:any, index:any) => (
+          <div key={index}>
+            <a
+              href={formatName.url}
+              target="_blank"
+              download
+              className=" outline-none italic underline"
+            >
+              {formatName.mimeType.split(";")[0] + "  "}
+              {formatName.hasVideo ? formatName.height + "p" : ""}
+            </a>
+          </div>
+        ))
+      }
+    </AspectRatio>
+        </> : <>  <Blockquote color="yellow" icon={icon} mt="xl">
+      Enter a youtube url to download
+    </Blockquote> </>}
+        
+        </Container>
+      </section>
+    </>
   );
 };
 
 export default About;
+
+{
+  /*  */
+}
+
+{
+  /* <div>
+      <iframe title="video" src={`${data?.data.url}`} />
+    </div>
+    <div>
+      {data?.data.info.map((formatName:any, index:any) => (
+        <div key={index}>
+          <a
+            href={formatName.url}
+            target="_blank"
+            download
+            className=" outline-none italic underline"
+          >
+            {formatName.mimeType.split(";")[0] + "  "}
+            {formatName.hasVideo ? formatName.height + "p" : ""}
+          </a>
+        </div>
+      ))}
+    </div> */
+}
--- src/api/download.js
+++ src/api/download.js
@@ -1,5 +1,36 @@
+import ytdl from "ytdl-core";
+import fs from "fs"
 export default async function GET(req , res){
-    console.log("hi")
-    console.log(req.query);
-    res.status(200).json({message:"ok"})
+    //console.log("hi")
+    const link = req.query.link;
+    if (!link) {
+        return res.json({ message: "No URL" });
+    }
+
+    try {
+        const videoId = await ytdl.getURLVideoID(link)
+        console.log("video id : " , videoId)
+        const metaInfo = await ytdl.getInfo(link)
+        let data = {
+            url: 'https://www.youtube.com/embed/'+videoId,
+            info: metaInfo.formats
+        }
+        return res.send(data)
+    } catch (error) {
+        console.log(error)
+        return res.json({message : "error"});
+    }
+   
+
+    // ytdl.getInfo(link).then((info)=>{
+    //     const format = ytdl.chooseFormat(info.formats,{quality:"248"});
+    //     const outputFilePath = `${info.videoDetails.title}`;
+    //     const outputStream = fs.createWriteStream(outputFilePath);
+    //     ytdl.downloadFromInfo(info, { format: format }).pipe(outputStream);
+    //     outputStream.on('finish', () => {
+    //         console.log(`Finished downloading: ${outputFilePath}`);
+    //       });
+    // }).catch((err) => {
+    //     console.error(err);
+    //   })
 }
