ea83666ba7c8bfdea22a9689368f74212c4cdf29
neeramrutia <neer.amrutia@gmail.com>
2024-07-14 17:00:34 +0530

    changed output links

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -148,17 +148,17 @@ const About = () => {
       />
       {
         data?.data.info.map((formatName:any, index:any) => (
-          <div key={index}>
+          <>
             <a
               href={formatName.url}
               target="_blank"
               download
-              className=" outline-none italic underline"
             >
               {formatName.mimeType.split(";")[0] + "  "}
               {formatName.hasVideo ? formatName.height + "p" : ""}
             </a>
-          </div>
+            <br/>
+            </>
         ))
       }
     </AspectRatio>
